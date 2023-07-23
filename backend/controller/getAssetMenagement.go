0e02ffad3d12419aa49002ecbd1a8f7f53992f42
world137 <world.sk137@gmail.com>
2023-07-23 14:10:32 +0700

    add api and show data

--- backend/controller/getAssetMenagement.go
+++ backend/controller/getAssetMenagement.go
@@ -1,81 +1,58 @@
-// package controller
-
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"io/ioutil"
-// 	"net/http"
-// )
-
-// type FunFact struct {
-// 	Last_upd_date string `json:"last_upd_date"`
-// 	Unique_id     string `json:"unique_id"`
-// 	Name_th       string `json:"name_th"`
-// 	Name_en       string `json:"name_en"`
-// }
-
-// func main() {
-// 	Get_all_asset_management()
-// }
-
-// func Get_all_asset_management() {
-// 	http.HandleFunc("/getAllAssetManagement/", func(w http.ResponseWriter, r *http.Request) {
-
-// 		if r.Method == http.MethodGet {
-// 			// var fact FunFact
-// 			apiUrl := "https://api.sec.or.th/FundFactsheet/fund/amc"
-// 			// เรียกใช้ฟังก์ชันเพื่อดึงข้อมูลจาก API
-// 			data, err := fetchAPI(apiUrl)
-// 			Fact := []FunFact{}
-// 			err = json.Unmarshal(data, &Fact)
-// 			if err == nil {
-// 				fmt.Errorf("error")
-
-// 			}
-// 			// Convert user data to JSON
-// 			userJSON, err := json.Marshal(Fact)
-// 			if err == nil {
-// 				http.Error(w, err.Error(), http.StatusInternalServerError)
-// 				return
-// 			}
-// 			// Send the JSON data as response
-// 			w.Header().Set("Content-Type", "application/json")
-// 			w.Write(userJSON)
-// 			return
-// 		}
-// 	})
-// }
-
-//	func fetchAPI(url string) ([]byte, error) {
-//		// ส่ง HTTP GET request ไปยัง URL ที่กำหนด
-//		response, err := http.Get(url)
-//		if err != nil {
-//			return nil, err
-//		}
-//		defer response.Body.Close()
-//		// อ่านข้อมูลที่ได้รับมา
-//		body, err := ioutil.ReadAll(response.Body)
-//		if err != nil {
-//			return nil, err
-//		}
-//		return body, nil
-//	}
 package controller
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
-type Data2 struct {
-	Status string `json:"status"`
+type FunFact struct {
+	Last_upd_date string `json:"last_upd_date"`
+	Unique_id     string `json:"unique_id"`
+	Name_th       string `json:"name_th"`
+	Name_en       string `json:"name_en"`
 }
 
-func API2Handler(w http.ResponseWriter, r *http.Request) {
-	data := Data2{
-		Status: "API 2 is up and running.",
-	}
+func GetAll() {
+	http.HandleFunc("/allAssetManagement/", func(w http.ResponseWriter, r *http.Request) {
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
+		if r.Method == http.MethodGet {
+			apiUrl := "https://api.sec.or.th/FundFactsheet/fund/amc"
+			// เรียกใช้ฟังก์ชันเพื่อดึงข้อมูลจาก API
+			data, err := fetchAPI(apiUrl)
+			Fact := []FunFact{}
+			err = json.Unmarshal(data, &Fact)
+			if err != nil {
+				fmt.Errorf("error")
+			}
+
+			// Convert user data to JSON
+			userJSON, err := json.Marshal(Fact)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			// Send the JSON data as response
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(userJSON)
+			return
+		}
+	})
+}
+
+func fetchAPI(url string) ([]byte, error) {
+	// ส่ง HTTP GET request ไปยัง URL ที่กำหนด
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	// อ่านข้อมูลที่ได้รับมา
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
 }
--- frontend/js/assetManagement.js
+++ frontend/js/assetManagement.js
@@ -1,45 +1,27 @@
 $(function () {
     $("#insert_navbar").load("../component/navbar.html");
-    $("#insert_snackbar").load("../component/snackbar.html");    
-    // get_all_fund_fact()
-    getData()
+    $("#insert_snackbar").load("../component/snackbar.html");
+    get_all_asset_management("C0000000021")
 })
 
-// function get_all_asset_management() {
-//     $.ajax({
-//         url: "/getAllAssetManagement/",
-//         type: "GET",
-//         dataType: "json",
-//         success: function(response) {
-//             // Display the user data in the userData div
-//             html = ""
-//             for (let index = 0; index < response.length; index++) {
-//                console.log(response[index])
-//                html = html + "<p>Name : " + response[index].name_th + "</p><p> Id: " + response[index].unique_id + "</p>"
-//             }
-//             $("#all_fund_fact").html(html)
-//             // $("#Funfact").html("<p>Name : " + unique.name_th + "</p><p>User Id: " + unique.unique_id + "</p>");
-//         },
-//         error: function(xhr, status, error) {
-//             error_notification('ไม่สามารถดึงข้อมูลได้')
-//             console.error("Error:", error);
-//         }
-//     });
-// }
-
-function getData() {
-    // Replace localhost:8080 with the address where your Golang server is running
-    const apiUrl1 = 'http://localhost:8080';
-    
-  
-    // Make an Ajax request to the Golang backend for API 1
-    fetch(apiUrl1)
-      .then(response => response.json())
-      .then(data => {
-        // Display the data in the 'result1' div
-        const resultDiv1 = document.getElementById('result1');
-        resultDiv1.innerHTML = data.message;
-      })
-      .catch(error => console.error('Error:', error));
-  
-  }
+function get_all_asset_management(id) {
+    $.ajax({
+        url: "/allAssetManagement/" + id,
+        type: "GET",
+        dataType: "json",
+        success: function (response) {
+            // Display the user data in the userData div
+            html = ""
+            for (let index = 0; index < response.length; index++) {
+                $("#all_asset_management_name").append("<div class='card' id='card" + response[index].unique_id + "'></div>")
+                $("#card" + response[index].unique_id).append("<div class='card-body'><div id='card_name'><h6>"+ response[index].name_th +"</h6><h6>"+ response[index].name_en +"</h6></div></div>")
+            }
+            // $("#all_fund_fact").html(html)
+            // $("#all_fund_fact").html("<p>Name : " + response[i].name_th + "</p><p>User Id: " + response[i].unique_id + "</p>");
+        },
+        error: function (xhr, status, error) {
+            error_notification('ไม่สามารถดึงข้อมูลได้')
+            console.error("Error:", error);
+        }
+    });
+}
