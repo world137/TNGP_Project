b72b9fc4e286d44b2ec6535bd5a37d7a08b80a1d
world137 <world.sk137@gmail.com>
2023-07-24 12:26:21 +0700

    add user profile

--- frontend/js/customerInfo.js
+++ frontend/js/customerInfo.js
@@ -1,29 +1,51 @@
 $(function () {
     $("#insert_navbar").load("../component/navbar.html");
     $("#insert_snackbar").load("../component/snackbar.html");
-    resiveCitizen()
+    showCustomerData()
 })
-function resiveCitizen() {
-    let citizenId = localStorage.getItem("citizenId")
-    console.log(citizenId)
-    show_customer_data(citizenId)
-}
+function showCustomerData() {
+    let name = localStorage.getItem("name")
+    let tel = localStorage.getItem("tel")
+    let email = localStorage.getItem("email")
+    let dob = localStorage.getItem("dob")
+    let job = localStorage.getItem("job")
+    let salary = localStorage.getItem("salary")
+    let risk = localStorage.getItem("risk")
+    let experienceTest = localStorage.getItem("experienceTest")
+    let experience = localStorage.getItem("experience")
 
-function show_customer_data(citizenId){
-    if(citizenId != null && citizenId != undefined){
-        $.ajax({
-            url: "/customerdata/" + citizenId,
-            type: "GET",
-            dataType: "json",
-            success: function (response) {
-                console.log(response)
-                console.log(response.name)
-                
-            },
-            error: function (xhr, status, error) {
-                error_notification('ไม่สามารถดึงข้อมูลได้')
-                console.error("Error:", error);
+    document.getElementById("name").innerHTML = name
+    document.getElementById("tel").innerHTML = tel
+    document.getElementById("email").innerHTML = email
+    document.getElementById("dob").innerHTML = dob
+    document.getElementById("job").innerHTML = job
+    document.getElementById("salary").innerHTML = salary
+
+
+
+    localStorage.clear
+
+    if (risk != "") {
+        if (risk == "0") {
+            document.getElementById("risk_data").innerHTML = "N/A"
+        } else {
+            document.getElementById("risk_data").innerHTML = "N/A"
+
+        }
+    }
+    if (experienceTest != "") {
+        if (experienceTest == "false") {
+            document.getElementById("experience_data").innerHTML = "N/A"
+        } else {
+            let experience = localStorage.getItem("experience")
+            if (experience == "true"){
+                document.getElementById("experience_data").innerHTML = "มีประสบการณ์การลงทุน"
+            }else{
+                document.getElementById("experience_data").innerHTML = "ไม่มีประสบการณ์การลงทุน"
             }
-        });
+
+        }
     }
 }
+
+
