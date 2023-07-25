deef371ba890657c2d2d329f3fc3bcaa5f2fad2a
6210612633 <60337843+6210612633@users.noreply.github.com>
2023-07-25 15:41:33 +0700

    Main page / footer / have nav bug on mainpage (#34)
    * index.html
    
    * insert footer/have main page nav bugs

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,8 +1,7 @@
 $(function () {
     $("#insert_navbar").load("./component/navbar.html");
     $("#insert_snackbar").load("./component/snackbar.html");
+    $("#body_page").load("./html/tutorial.html");
 })
 
-$("#find_customer").on("click",function () {
-    window.location.href = "../../frontend/html/findCustomer.html"
-})
+
