9dfcbed178938d333b694d74eec4fa4c4051b551
world.cyp <59548715+world137@users.noreply.github.com>
2023-07-25 08:31:47 +0700

    fix bug (#25)

--- frontend/js/allFund.js
+++ frontend/js/allFund.js
@@ -1,11 +1,11 @@
 $(function () {
     $("#insert_navbar").load("../component/navbar.html");
     $("#insert_snackbar").load("../component/snackbar.html");
-    if(localStorage.getItem("asset_management_id") === null){
+    if (localStorage.getItem("asset_management_id") === null) {
         window.location.href = "../../frontend/html/assetManagement.html"
     }
     let asset_management_id = localStorage.getItem("asset_management_id")
-    
+
     localStorage.clear()
     show_all_fund(asset_management_id)
 })
@@ -23,31 +23,24 @@ function show_all_fund(asset_management_id) {
                 data = response;
                 totalCards = response.length;
 
-                // Calculate the start and end indexes for the current page
                 var startIndex = (currentPage - 1) * cardsPerPage;
                 var endIndex = Math.min(startIndex + cardsPerPage, totalCards);
 
-                // Clear the card container before adding new cards
                 $("#all_fund_name").empty();
 
-                // Create a string to hold the HTML for all the cards on the current page
                 var cardsHtml = "";
 
                 for (let index = startIndex; index < endIndex; index++) {
-                    // Create HTML for each card and append it to the cardsHtml string
                     cardsHtml += "<div onclick=send_proj_id('" + data[index].proj_id + "') class='card' id='card" + data[index].proj_id + "'>";
                     cardsHtml += "<div class='card-body'><div id='card_name'><h6>" + data[index].proj_name_th + "</h6><h6>" + data[index].proj_name_en + "</h6></div></div>";
                     cardsHtml += "</div>";
                 }
 
-                // Append all the cards HTML to the card container
                 $("#all_fund_name").append(cardsHtml);
 
-                // Update the page info
                 var totalPages = Math.ceil(totalCards / cardsPerPage);
                 $("#page_info").text("Page " + currentPage + " of " + totalPages);
 
-                // Show or hide the "Previous" and "Next" buttons based on the current page and total number of cards
                 if (currentPage > 1) {
                     document.getElementById("prev_button1").disabled = false;
                     document.getElementById("prev_button2").disabled = false;
@@ -56,17 +49,16 @@ function show_all_fund(asset_management_id) {
                     document.getElementById("prev_button1").disabled = true;
                     document.getElementById("prev_button2").disabled = true;
                 }
-    
+
                 if (endIndex < totalCards) {
                     document.getElementById("next_button1").disabled = false;
                     document.getElementById("next_button2").disabled = false;
                 } else {
                     document.getElementById("next_button1").disabled = true;
                     document.getElementById("next_button2").disabled = true;
-    
+
                 }
 
-                // Show the current page number
                 $("#current_page1").text(currentPage + "/" + totalPages);
                 $("#current_page2").text(currentPage + "/" + totalPages);
             },
@@ -84,37 +76,28 @@ function search_by_card_name(searchTerm) {
         return cardName.includes(searchTerm);
     });
 
-    // Update the current page to 1 since the search results start from the first page
     currentPage = 1;
 
-    // Update the totalCards with the number of matching cards
     totalCards = matchingCards.length;
 
-    // Calculate the start and end indexes for the current page
     var startIndex = (currentPage - 1) * cardsPerPage;
     var endIndex = Math.min(startIndex + cardsPerPage, totalCards);
 
-    // Clear the card container before adding new cards
     $("#all_fund_name").empty();
 
-    // Create a string to hold the HTML for all the cards on the current page
     var cardsHtml = "";
 
     for (let index = startIndex; index < endIndex; index++) {
-        // Create HTML for each card and append it to the cardsHtml string
         cardsHtml += "<div onclick=send_proj_id('" + matchingCards[index].proj_id + "') class='card' id='card" + matchingCards[index].proj_id + "'>";
         cardsHtml += "<div class='card-body'><div id='card_name'><h6>" + matchingCards[index].proj_name_th + "</h6><h6>" + matchingCards[index].proj_name_en + "</h6></div></div>";
         cardsHtml += "</div>";
     }
 
-    // Append all the cards HTML to the card container
     $("#all_fund_name").append(cardsHtml);
 
-    // Update the page info
     var totalPages = Math.ceil(totalCards / cardsPerPage);
     $("#page_info").text("Page " + currentPage + " of " + totalPages);
 
-    // Show or hide the "Previous" and "Next" buttons based on the current page and total number of cards
     if (currentPage > 1) {
         document.getElementById("prev_button1").disabled = false;
         document.getElementById("prev_button2").disabled = false;
@@ -131,12 +114,10 @@ function search_by_card_name(searchTerm) {
         document.getElementById("next_button2").disabled = true;
     }
 
-    // Show the current page number
     $("#current_page1").text(currentPage);
     $("#current_page2").text(currentPage);
 }
 
-// Add event listener for search button
 $("#search_button").on("click", function () {
     var searchTerm = $("#search_input").val().toLowerCase();
     search_by_card_name(searchTerm);
@@ -149,7 +130,6 @@ $("#next_button1").on("click", function () {
     }
 });
 
-// Event listener for the "Previous" button to load the previous set of cards
 $("#prev_button1").on("click", function () {
     if (currentPage > 1) {
         currentPage--;
@@ -163,7 +143,6 @@ $("#next_button2").on("click", function () {
     }
 });
 
-// Event listener for the "Previous" button to load the previous set of cards
 $("#prev_button2").on("click", function () {
     if (currentPage > 1) {
         currentPage--;
@@ -171,14 +150,12 @@ $("#prev_button2").on("click", function () {
     }
 });
 
-// Initial load of the data
 show_all_fund(localStorage.getItem("asset_management_id"));
 
 function send_proj_id(proj_id) {
     var currentdate = new Date();
     var today = currentdate.getFullYear() + "-" + (currentdate.getMonth() + 1) + "-" + currentdate.getDate()
     localStorage.setItem("proj_id", proj_id)
-    // localStorage.setItem("nav_date", today)
     localStorage.setItem("nav_date", "2023-07-20")
     window.location.href = "../../frontend/html/fundDetail.html" //
 }
--- frontend/js/assetManagement.js
+++ frontend/js/assetManagement.js
@@ -17,30 +17,23 @@ function get_all_asset_management() {
             data = response;
             totalCards = response.length;
 
-            // Calculate the start and end indexes for the current page
             var startIndex = (currentPage - 1) * cardsPerPage;
             var endIndex = Math.min(startIndex + cardsPerPage, totalCards);
 
-            // Clear the card container before adding new cards
             $("#all_asset_management_name").empty();
 
-            // Create a string to hold the HTML for all the cards on the current page
             var cardsHtml = "";
 
             for (let index = startIndex; index < endIndex; index++) {
-                // Create HTML for each card and append it to the cardsHtml string
                 cardsHtml += "<div onclick=show_all_fund('" + data[index].unique_id + "') class='card' id='card" + data[index].unique_id + "'>";
                 cardsHtml += "<div class='card-body'><div id='card_name'><h6>" + data[index].name_th + "</h6><h6>" + data[index].name_en + "</h6></div></div>";
                 cardsHtml += "</div>";
             }
 
-            // Append all the cards HTML to the card container
             $("#all_asset_management_name").append(cardsHtml);
 
-            // Update the page info
             var totalPages = Math.ceil(totalCards / cardsPerPage);
 
-            // Show or hide the "Previous" and "Next" buttons based on the current page and total number of cards
             if (currentPage > 1) {
                 document.getElementById("prev_button1").disabled = false;
                 document.getElementById("prev_button2").disabled = false;
@@ -61,7 +54,6 @@ function get_all_asset_management() {
 
             }
 
-            // Show the current page number
             $("#current_page1").text(currentPage + "/" + totalPages);
             $("#current_page2").text(currentPage + "/" + totalPages);
 
@@ -79,36 +71,27 @@ function search_by_card_name(searchTerm) {
         return cardName.includes(searchTerm);
     });
 
-    // Update the current page to 1 since the search results start from the first page
     currentPage = 1;
 
-    // Update the totalCards with the number of matching cards
     totalCards = matchingCards.length;
 
-    // Calculate the start and end indexes for the current page
     var startIndex = (currentPage - 1) * cardsPerPage;
     var endIndex = Math.min(startIndex + cardsPerPage, totalCards);
 
-    // Clear the card container before adding new cards
     $("#all_asset_management_name").empty();
 
-    // Create a string to hold the HTML for all the cards on the current page
     var cardsHtml = "";
 
     for (let index = startIndex; index < endIndex; index++) {
-        // Create HTML for each card and append it to the cardsHtml string
         cardsHtml += "<div onclick=show_all_fund('" + matchingCards[index].unique_id + "') class='card' id='card" + matchingCards[index].unique_id + "'>";
         cardsHtml += "<div class='card-body'><div id='card_name'><h6>" + matchingCards[index].name_th + "</h6><h6>" + matchingCards[index].name_en + "</h6></div></div>";
         cardsHtml += "</div>";
     }
 
-    // Append all the cards HTML to the card container
     $("#all_asset_management_name").append(cardsHtml);
 
-    // Update the page info
     var totalPages = Math.ceil(totalCards / cardsPerPage);
 
-    // Show or hide the "Previous" and "Next" buttons based on the current page and total number of cards
     if (currentPage > 1) {
         document.getElementById("prev_button1").disabled = false;
         document.getElementById("prev_button2").disabled = false;
@@ -125,12 +108,10 @@ function search_by_card_name(searchTerm) {
         document.getElementById("next_button2").disabled = true;
     }
 
-    // Show the current page number
     $("#current_page1").text(currentPage);
     $("#current_page2").text(currentPage);
 }
 
-// Add event listener for search button
 $("#search_button").on("click", function () {
     var searchTerm = $("#search_input").val().toLowerCase();
     search_by_card_name(searchTerm);
@@ -142,7 +123,6 @@ $("#next_button1").on("click", function () {
     }
 });
 
-// Event listener for the "Previous" button to load the previous set of cards
 $("#prev_button1").on("click", function () {
     if (currentPage > 1) {
         currentPage--;
@@ -156,17 +136,14 @@ $("#next_button2").on("click", function () {
     }
 });
 
-// Event listener for the "Previous" button to load the previous set of cards
 $("#prev_button2").on("click", function () {
     if (currentPage > 1) {
         currentPage--;
         get_all_asset_management();
     }
 });
-// Initial load of the data
 get_all_asset_management();
 
-// Initial load of the data
 get_all_asset_management();
 function show_all_fund(asset_management_id, asset_management_name) {
     localStorage.setItem("asset_management_id", asset_management_id)
