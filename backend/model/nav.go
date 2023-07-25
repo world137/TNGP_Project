6e1164b9cf0978e16aa218e5f77d0495f3a08ad8
jirat sukphalanon <jirat.su@tnds.com>
2023-07-25 22:44:53 +0700

    handle NaN

--- backend/model/nav.go
+++ backend/model/nav.go
@@ -1,9 +1,14 @@
 package model
 
 type NAV struct {
-	Last_upd_date string  `json:"last_upd_date"`
-	Nav_date      string  `json:"nav_date"`
-	Net_asset     float64 `json:"net_asset"`
-	Last_val      float64 `json:"last_val"`
-	Previous_val  float64 `json:"previous_val"`
+	Last_upd_date string     `json:"last_upd_date"`
+	Nav_date      string     `json:"nav_date"`
+	Net_asset     float64    `json:"net_asset"`
+	Last_val      float64    `json:"last_val"`
+	Previous_val  float64    `json:"previous_val"`
+	Amc_info      []Amc_info `json:"amc_info"`
+}
+
+type Amc_info struct {
+	Sell_price float64 `json:"sell_price"`
 }
